e07626f4978e3386dcf242e13bbe478a4306751c
Nishanthartham <arthamnishanth123@gmail.com>
2024-08-05 23:16:56 +0530

    HYDDR81-ni-wp-sp-clone-2.py

--- custom-files/reward-fn-examples/HYDDR81-ni-wp-sp-clone-2.py
+++ custom-files/reward-fn-examples/HYDDR81-ni-wp-sp-clone-2.py
@@ -15,9 +15,9 @@ def reward_function(params):
 
     # Speed
 
-    # fast = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22, 23, 24, 25, 26, 27,34, 35, 36, 37, 38, 39,40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,59, 60, 61, 62, 63, 64,70, 71, 72, 73, 74, 75, 76, 77,99,100, 101, 102, 103, 104, 105, 106, 107, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131,132,133,134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154]  # 3
+    fast = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,12, 13,14, 15, 16, 17, 18,19,20,21,22, 23, 24, 25, 26, 27,28,29,30,31,32,33,34, 35, 36, 37, 38, 39,40, 41,54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,65, 66,80, 81, 82, 83,84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131,132,133,134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154]  # 3
 
-    # moderate = [19,20,21,31,32,33,84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,108, 109, 110, 111, 112, 113, 114, 115,116,117]  # 2
+    moderate = [11,42,43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 67, 68, 69,70, 71, 72, 73, 74, 75, 76, 77,78,79,99, 100, 101, 102, 103, 104, 105, 106]  # 2
 
     # slow = [14, 15, 16, 17, 18,28,29,30,54, 55, 56, 57, 58, 59,65, 66, 67, 68, 69,78, 79,80, 81, 82, 83]  # 1
     # sharp_turns = [57, 69,80]
@@ -50,37 +50,36 @@ def reward_function(params):
         reward -= 10
 
 ###rewarding to prevent zigzag
-    # if abs(params["steering_angle"]) < 10:
+    # if abs(params["steering_angle"]) < 10:( / params["steps"]
     #     reward += 1
+    step = params["steps"]
     if step > 0:
-        reward = ((progress*150)/step)**2
-    else:
-        reward = 1
+        reward = ((params["progress"]*150)/step)**2
 # # Steering penality threshold, change the number based on your action space setting
 #     ABS_STEERING_THRESHOLD = 15
 #     if steering > ABS_STEERING_THRESHOLD:
 #         reward *= 0.8
 #####SPEED
 
-    # if params["closest_waypoints"][1] in fast:
+    if params["closest_waypoints"][1] in fast:
 
-    #     if params["speed"] > 2.9:
+        if params["speed"] > 2.75:
 
-    #         reward += 20
+            reward += 20
 
-    #     else:
+        else:
 
-    #         reward -= 20
+            reward -= 20
 
-    # elif params["closest_waypoints"][1] in moderate:
+    elif params["closest_waypoints"][1] in moderate:
 
-    #     if params["speed"] > 1 and params["speed"] <= 3.3:
+        if params["speed"] > 1 and params["speed"] <= 2.75:
 
-    #         reward += 10
+            reward += 10
 
-    #     else:
+        else:
 
-    #         reward -= 10
+            reward -= 10
 
     # elif params["closest_waypoints"][1] in slow:
 
@@ -109,3 +108,7 @@ def reward_function(params):
     # print("Is left", params["is_left_of_center"])
 
     return float(reward)
+
+
+# params = {"speed":1,"steering_angle":90,"steps":10,"distance_from_center":90,"heading":8,"waypoints":[[1,2],[1,2],[1,2]],"closest_waypoints":[0,0],"track_width":10,"all_wheels_on_track":True,"is_left_of_center":True,"progress":10}
+# print(reward_function(params))
--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,110 +1,114 @@
-import math
-
-class PARAMS:
-    prev_speed = None
-    prev_steering_angle = None 
-    prev_steps = None
-    prev_direction_diff = None
-    prev_normalized_distance_from_route = None
-
-def calculate_track_direction(waypoints, closest_waypoints):
-    next_point = waypoints[closest_waypoints[1]]
-    prev_point = waypoints[closest_waypoints[0]]
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    track_direction = math.degrees(track_direction)
-    return track_direction
-
-def calculate_normalized_distance_from_route(distance_from_center, track_width):
-    return distance_from_center / (track_width / 2)
-
-def is_heading_in_right_direction(heading, track_direction, threshold=10):
-    direction_diff = abs(track_direction - heading)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    return direction_diff < threshold
-
-def is_turn_upcoming(waypoints, closest_waypoints, turn_threshold=15):
-    next_point = waypoints[closest_waypoints[1]]
-    future_point = waypoints[min(closest_waypoints[1] + 5, len(waypoints) - 1)]
-    future_direction = math.atan2(future_point[1] - next_point[1], future_point[0] - next_point[0])
-    future_direction = math.degrees(future_direction)
-    track_direction = calculate_track_direction(waypoints, closest_waypoints)
-    direction_diff = abs(track_direction - future_direction)
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    return direction_diff > turn_threshold
-
 def reward_function(params):
-    # Read input parameters
-    heading = params['heading']
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steps = params['steps']
-    steering_angle = params['steering_angle']
-    speed = params['speed']
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
+    center_variance = params["distance_from_center"] / params["track_width"]
+    steering = abs(params['steering_angle'])
+    # racing line 84 points
+# #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+# 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+#  60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 
+#   100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115,
+#    116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 
+#    132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155]
+    left_lane = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21, 22, 23,24, 25, 26, 27,28,29,30,31,32,33,34, 35, 36, 37, 38, 39,40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93,90, 91, 92, 93, 94, 95, 96, 97, 98, 99,100, 101, 102, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155]
 
-    track_direction = calculate_track_direction(waypoints, closest_waypoints)
-    direction_diff = abs(track_direction - heading)
-    normalized_distance_from_route = calculate_normalized_distance_from_route(distance_from_center, track_width)
-    heading_right_direction = is_heading_in_right_direction(heading, track_direction)
-    turn_upcoming = is_turn_upcoming(waypoints, closest_waypoints)
+    center_lane = [103,104, 105, 106, 107, 108, 109, 110, 111, 112, 113,114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131]  # Fill in the waypoints
+    # right_lane = [ 41, 42, 43, 44, 45, 46, 47,66, 67, 68, 69, 70,76, 77, 78, 79,80, 81, 82, 83]  # Fill in the waypoint4
 
-    # Reinitialize previous parameters if it is a new episode
-    if PARAMS.prev_steps is None or steps < PARAMS.prev_steps:
-        PARAMS.prev_speed = None
-        PARAMS.prev_steering_angle = None
-        PARAMS.prev_direction_diff = None
-        PARAMS.prev_normalized_distance_from_route = None
-    
-    # Check if the speed has dropped
-    has_speed_dropped = False
-    if PARAMS.prev_speed is not None and PARAMS.prev_speed > speed:
-        has_speed_dropped = True
-    
-    # Penalize slowing down without good reason on straight portions
-    speed_maintain_bonus = 1
-    if has_speed_dropped and not turn_upcoming:
-        speed_maintain_bonus = min(speed / PARAMS.prev_speed, 1)
-    
-    # Penalize making the heading direction worse
-    heading_decrease_bonus = 0
-    if PARAMS.prev_direction_diff is not None and heading_right_direction:
-        if abs(PARAMS.prev_direction_diff / direction_diff) > 1:
-            heading_decrease_bonus = min(10, abs(PARAMS.prev_direction_diff / direction_diff))
-    
-    # Check if the steering angle has changed
-    has_steering_angle_changed = False
-    if PARAMS.prev_steering_angle is not None and not math.isclose(PARAMS.prev_steering_angle, steering_angle):
-        has_steering_angle_changed = True
-    
-    steering_angle_maintain_bonus = 1
-    if heading_right_direction and not has_steering_angle_changed:
-        if abs(direction_diff) < 10:
-            steering_angle_maintain_bonus *= 2
-        if abs(direction_diff) < 5:
-            steering_angle_maintain_bonus *= 2
-        if PARAMS.prev_direction_diff is not None and abs(PARAMS.prev_direction_diff) > abs(direction_diff):
-            steering_angle_maintain_bonus *= 2
-    
-    # Reward reducing distance to the race line
-    distance_reduction_bonus = 1
-    if PARAMS.prev_normalized_distance_from_route is not None and PARAMS.prev_normalized_distance_from_route > normalized_distance_from_route:
-        if abs(normalized_distance_from_route) > 0:
-            distance_reduction_bonus = min(abs(PARAMS.prev_normalized_distance_from_route / normalized_distance_from_route), 2)
-    
-    reward = speed_maintain_bonus + heading_decrease_bonus + steering_angle_maintain_bonus + distance_reduction_bonus
-    
-    # Before returning reward, update the variables
-    PARAMS.prev_speed = speed
-    PARAMS.prev_steering_angle = steering_angle
-    PARAMS.prev_direction_diff = direction_diff
-    PARAMS.prev_steps = steps
-    PARAMS.prev_normalized_distance_from_route = normalized_distance_from_route
-    
-    return reward
+    # Speed
+
+    fast = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,12, 13,14, 15, 16, 17, 18,19,20,21,22, 23, 24, 25, 26, 27,28,29,30,31,32,33,34, 35, 36, 37, 38, 39,40, 41,54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,65, 66,80, 81, 82, 83,84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131,132,133,134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154]  # 3
+
+    moderate = [11,42,43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 67, 68, 69,70, 71, 72, 73, 74, 75, 76, 77,78,79,99, 100, 101, 102, 103, 104, 105, 106]  # 2
+
+    # slow = [14, 15, 16, 17, 18,28,29,30,54, 55, 56, 57, 58, 59,65, 66, 67, 68, 69,78, 79,80, 81, 82, 83]  # 1
+    # sharp_turns = [57, 69,80]
+    # rounded_turns = [19, 32, 107]
+
+    reward = 30
+
+    if params["all_wheels_on_track"]:
+
+        reward += 5
+
+    else:
+
+        reward -= 10
+
+    if params["closest_waypoints"][1] in left_lane and params["is_left_of_center"]:
+
+        reward += 10
+
+    # elif params["closest_waypoints"][1] in right_lane and not params["is_left_of_center"]:
+
+    #     reward += 10
+
+    elif params["closest_waypoints"][1] in center_lane and center_variance < 0.4:
+
+        reward += 10
+
+    else:
+
+        reward -= 10
+
+###rewarding to prevent zigzag
+    # if abs(params["steering_angle"]) < 10:( / params["steps"]
+    #     reward += 1
+    step = params["steps"]
+    if step > 0:
+        reward = ((params["progress"]*150)/step)**2
+# # Steering penality threshold, change the number based on your action space setting
+#     ABS_STEERING_THRESHOLD = 15
+#     if steering > ABS_STEERING_THRESHOLD:
+#         reward *= 0.8
+#####SPEED
+
+    if params["closest_waypoints"][1] in fast:
+
+        if params["speed"] > 2.75:
+
+            reward += 20
+
+        else:
+
+            reward -= 20
+
+    elif params["closest_waypoints"][1] in moderate:
+
+        if params["speed"] > 1 and params["speed"] <= 2.75:
+
+            reward += 10
+
+        else:
+
+            reward -= 10
+
+    # elif params["closest_waypoints"][1] in slow:
+
+    #     if params["speed"] <= 1:
+
+    #         reward += 10
+
+    #     else:
+
+    #         reward -= 10
+
+    # if params["closest_waypoints"][1] in sharp_turns:
+    #     if params['steering_angle'] > 20 and params['steering_angle'] < 50:
+    #         reward += 25
+    #     else:
+    #         reward -= 25
+    #
+    # if params["closest_waypoints"][1] in rounded_turns:
+    #     if params['steering_angle'] > 10 and params['steering_angle'] < 20:
+    #         reward += 25
+    #     else:
+    #         reward -= 25
+
+    # print("Next point", params["closest_waypoints"][1])
+    # print("Speed", params["speed"])
+    # print("Is left", params["is_left_of_center"])
+
+    return float(reward)
 
 
-# params = {"speed":1,"steering_angle":90,"steps":10,"distance_from_center":90,"heading":8,"waypoints":[[1,2],[1,2],[1,2]],"closest_waypoints":[0,0],"track_width":10}
+# params = {"speed":1,"steering_angle":90,"steps":10,"distance_from_center":90,"heading":8,"waypoints":[[1,2],[1,2],[1,2]],"closest_waypoints":[0,0],"track_width":10,"all_wheels_on_track":True,"is_left_of_center":True,"progress":10}
 # print(reward_function(params))
