326197c3d64c3fe3ed43234b2f785ff7256df39f
Nishanthartham <arthamnishanth123@gmail.com>
2024-08-08 12:03:06 +0530

    commit

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -12,7 +12,7 @@ def reward_function(params):
 
     center_lane = [103,104, 105, 106, 107, 108, 109, 110, 111, 112, 113,114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131]  # Fill in the waypoints
     right_lane = [ 57, 58, 59,60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80]  # Fill in the waypoint4
-    right_turn = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]
+    right_turn = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,80,81,82,83]
 
     # Speed
 
@@ -21,8 +21,8 @@ def reward_function(params):
     moderate = [11,42,43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 67, 68, 69,70, 71, 72, 73, 74, 75, 76, 77,78,79,99, 100, 101, 102, 103, 104, 105, 106]  # 2
 
     # slow = [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 73]  # 1
-    sharp_turns = [65,66,67]
-    left_rounded_turns = [11,12,13,48,49,50,51]
+    sharp_turns = [62,63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79]
+    left_rounded_turns = [48,49,50,51,106,107,108]
 
     reward = 30
 
@@ -41,7 +41,7 @@ def reward_function(params):
 
     elif params["closest_waypoints"][1] in right_lane and not params["is_left_of_center"]:
 
-        reward += 10
+        reward += 15
 
     elif params["closest_waypoints"][1] in center_lane and center_variance < 0.4:
         print("center lane")
@@ -95,7 +95,7 @@ def reward_function(params):
 
     if params["closest_waypoints"][1] in right_turn:
         print("in right turn")
-        if params["steering_angle"] < -5:
+        if params["steering_angle"] <= -5:
             reward -= 10
         else:
             reward += 10
@@ -108,17 +108,18 @@ def reward_function(params):
     else:
         print("in left turn")
 
-        if params["steering_angle"] > 5:
+        if params["steering_angle"] >= 5:
             reward -= 10
-        
+        else:
+            reward += 10
+
         if params["closest_waypoints"][1] in left_rounded_turns:
-            if params["steering_angle"] >= 15:
+            if params["steering_angle"] <= -10:
                 reward += 10
             else:
                 reward -= 10
 
-        else:
-            reward += 10
+
 
 
 
@@ -157,5 +158,5 @@ def reward_function(params):
     return float(reward)
 
 
-# params = {"speed":2.75,"steering_angle":0,"steps":71,"distance_from_center":90,"heading":8,"waypoints":[[1,2],[1,2],[1,2]],"closest_waypoints":[1,11],"track_width":10,"all_wheels_on_track":True,"is_left_of_center":True,"progress":10.61311464}
-# print(reward_function(params))
+# params = {"speed":2.75,"seering_angle":0,"steps":71,"distance_from_center":90,"heading":8,"waypoints":[[1,2],[1,2],[1,2]],"closest_waypoints":[1,11],"track_width":10,"all_wheels_on_track":True,"is_left_of_center":True,"progress":10.61311464}
+# print(reward_function(params))t
